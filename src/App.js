4406d5ad0d5cd96b2f8c01f8a52937eb14f6d138
Philemon Wachara <wachara@Renees-MacBook-Air.local>
2022-10-30 16:09:33 +0300

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,57 @@
-import logo from './logo.svg';
-import './App.css';
+
+import { DragDropContext, Droppable, Draggable } from 'react-beautiful-dnd';
 
 function App() {
+
+  const data = [
+    { name: "James Brown", age: 28, id: 'one' },
+    { name: "John Doe", age: 67 , id: 'two'},
+    { name: "Peter Parker", age: 45, id: 'three' },
+    { name: "Ryan Gray", age: 34 , id: 'four'},
+    { name: "Zediel Nielson", age: 52, id: 'five'},
+  ]
+
+  const onDragEnd = (result) => {
+    console.log(result)
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <DragDropContext onDragEnd={ onDragEnd }>
+        <Droppable droppableId='droppableArea'>
+          { (provided) => (
+
+            <div 
+              {...provided.droppableProps} 
+              ref={provided.innerRef} >
+
+              { data.map((person, index) => (
+                <Draggable 
+                  draggableId={ person.id.toString() }
+                  key={ person.id }
+                  index={index}
+                  >
+                  {(provided) => (
+                    <div 
+                      ref={provided.innerRef}
+                      {...provided.dragHandleProps}
+                      {...provided.draggableProps}
+                      
+                    >
+                      { person.name }
+                    </div>
+                  )}
+                </Draggable>
+
+              ))
+
+              }
+              { provided.placeholder }
+            </div>
+            
+          )}
+        </Droppable>
+      </DragDropContext>
     </div>
   );
 }
